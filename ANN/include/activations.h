2e3da3c08f603321002931979caf782c10ec711a
Oluwaseun Ojeleye <jimmystar14.ojs@gmail.com>
2020-02-06 02:33:58 +0300

    Update

--- ANN/include/activations.h
+++ ANN/include/activations.h
@@ -1,18 +1,8 @@
 #ifndef ACTIVATIONS_H_
 #define ACTIVATIONS_H_
 #include "matrix.h"
-
-//passing activation function as argument to operate on matrix
-template <class S> 
-Matrix<double> Activation_Function(S function, const Matrix<double> &matrix){
-    Matrix<double> Result(matrix.get_Row(), matrix.get_Column());
-    for(int i=0; i<matrix.get_Row(); i++){
-        for(int j=0; j<matrix.get_Column(); j++){
-            Result.set_Element(i, j, function(matrix.get_Element(i, j)));
-        }
-    }
-    return Result;
-}
+#include <functional>
+#include <map>
 
 // Activation Functions prototype declaration
 double sigmoid(const double param);  
@@ -21,5 +11,7 @@ double sigmoid_derivative(const double param);
 double relu(const double param);  
 double relu_derivative(const double param);
 
-#endif
+Matrix<double> Activation_Function(const std::string &string, const Matrix<double> &matrix);
 
+
+#endif
--- ANN/main.cpp
+++ ANN/main.cpp
@@ -17,21 +17,28 @@ int main(){
 
     Matrix<double> Mat3=Activation_Function(relu, Mat2);
     Mat3.print();*/
+
 	/*std::vector<std::vector<double>> inputs({{252, 4, 155, 175}, {175, 10, 186, 200}, {82, 131, 230, 100}, {115, 138, 80, 88}});
 	std::vector<std::vector<double>> outputs({{1,0}, {1,0}, {0,1}, {0,1}});
 */
+	/*std::vector<std::vector<double>> inputs({{252, 4, 155, 175}, {175, 10, 186, 200}, {82, 131, 230, 100}, {115, 138, 80, 88}});
+	std::vector<std::vector<double>> outputs({{1,0}, {1,0}, {0,1}, {0,1}});*/
+
 	std::vector<std::vector<double>> inputs({{0, 0}, {0, 1}, {1, 0}, {1, 1}});
 	std::vector<std::vector<double>> outputs({{0}, {1}, {1}, {0}});
-	//Matrix<double> Y_train({0, 0, 1, 1});
-	std::vector<int> layers={2,1};
+
+	/*std::vector<std::vector<double>> inputs({{0, 0}, {0, 1}, {1, 0}, {1, 1}});
+	std::vector<std::vector<double>> outputs({{0,1}, {1, 0}, {1, 0}, {0, 1}});
+*/
+	std::vector<int> layers={2,2,1};
     NeuralNetwork Network1(layers);
 
     
-    for(int i=1; i<=3; i++){
+    for(int i=1; i<=20; i++){
     	for (int j=0; j<4; j++){
-    		Matrix<double> X_train(inputs[i]);
-    		Matrix<double> Y_train(outputs[i]);
-    		Network1.Train(X_train, Y_train, 0.5, "a", "b");
+    		Matrix<double> X_train(inputs[j]);
+    		Matrix<double> Y_train(outputs[j]);
+    		Network1.Train(X_train, Y_train, 0.5, "sigmoid", "sigmoid");
     	}
     }
 
@@ -40,7 +47,11 @@ int main(){
     	Matrix<double> X_train(inputs[i]);
     	Network1.Predict(X_train).print();
     }
+
+    Network1.printWeight();
     
+
+
 
     return 0;
 }
